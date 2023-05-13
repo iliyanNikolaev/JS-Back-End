ad55352de567f5db9e66685d4004c991748f74cc
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-13 19:39:04 +0300

    cat-shelter

---
+++ Node.js-streams-and-utilities/cat-shelter/JSviews/index.js
@@ -0,0 +1,55 @@
+const cats = require('../cats.json');
+const homePage = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../content/styles/site.css">
+    <link rel="shortcut icon" type="image/png" href="../../content/images/pawprint.ico" />
+    <title>Cat Shelter</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="navigation">
+                <li><a href="/">Home Page</a></li>
+                <li><a href="/cats/add-breed">Add Breed</a></li>
+                <li><a href="/cats/add-cat">Add Cat</a></li>
+            </ul>
+        </nav>
+        <h1>Cat Shelter</h1>
+        <form action="/search">
+            <input type="text">
+            <button type="button">Search</button>
+        </form>
+    </header>
+
+    <main>
+        <section class="cats">
+            <ul>
+            ${cats.map(x => `
+            <li>
+                    <img src="${x.img}" alt="Black Cat">
+                    <h3>${x.name}</h3>
+                    <p><span>Breed: </span>${x.breed}</p>
+                    <p><span>Description: </span>${x.description}</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="/edit">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+            `).join('\n')}
+            </ul>
+        </section>
+    </main>
+
+</body>
+
+</html>
+`
+
+module.exports = homePage;
--- Node.js-streams-and-utilities/cat-shelter/index.js
+++ Node.js-streams-and-utilities/cat-shelter/index.js
@@ -1,11 +1,25 @@
+const homePage = require('./JSviews/index');
+const editPage = require('./JSviews/edit');
+const siteCSS = require('./JScss/siteCSS');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     })
+
     switch(req.url){
         case '/':
+            res.write(homePage);
+            break;
+        case '/content/styles/site.css':
+            res.writeHead(200, {
+                'Content-Type': 'text/css'
+            })
+            res.write(siteCSS); 
+            break;
+        case '/edit': 
+            res.write(editPage);
             break;
         default:
             res.write(`
@@ -16,5 +30,5 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen(61080);
-console.log('Server is listening on port 61080...');
+server.listen(5000);
+console.log('Server is listening on port 5000...');
