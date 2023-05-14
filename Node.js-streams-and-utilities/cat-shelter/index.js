5f2bde746a51ee2ea9d54e056ffbe915384dee49
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-14 14:39:59 +0300

    demo how to use fs.readFIle

--- Node.js-streams-and-utilities/cat-shelter/index.js
+++ Node.js-streams-and-utilities/cat-shelter/index.js
@@ -1,34 +1,41 @@
-const homePage = require('./JSviews/index');
-const editPage = require('./JSviews/edit');
-const siteCSS = require('./JScss/siteCSS');
 const http = require('http');
+const fs = require('fs/promises');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     })
 
-    switch(req.url){
-        case '/':
-            res.write(homePage);
-            break;
-        case '/content/styles/site.css':
-            res.writeHead(200, {
-                'Content-Type': 'text/css'
-            })
-            res.write(siteCSS); 
-            break;
-        case '/edit': 
-            res.write(editPage);
-            break;
-        default:
-            res.write(`
-                <h1>404</h1>
-            `)
+    if(req.url == '/') {
+        const homePage = await readFromFile('./views/home.html');        
+        res.write(homePage);
+
+    } else if(req.url == '/content/styles/site.css'){
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        })
+        const siteCSS = await readFromFile('./content/styles/site.css');
+        res.write(siteCSS);
+
+    } else if(req.url == '/edit'){
+        const editPage = await readFromFile('./views/editCat.html');
+        res.write(editPage);
+    } else if(req.url == '/create'){
+        const createPage = await readFromFile('./views/addCat.html');
+        res.write(createPage);
+    } else {
+
+        res.write('<h1>404</h1>')
     }
 
     res.end();
 });
+
+
+async function readFromFile(path) {
+    return fs.readFile(path, { encoding: 'utf-8' });
+}
 
 server.listen(5000);
 console.log('Server is listening on port 5000...');
