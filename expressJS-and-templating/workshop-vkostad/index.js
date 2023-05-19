b7588aafe83b8a283dbd98fe485d6ba9063a903e
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-19 16:13:04 +0300

    created app scaffold

--- expressJS-and-templating/workshop-vkostad/index.js
+++ expressJS-and-templating/workshop-vkostad/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const hbs = require('express-handlebars').create({
+    extname: '.hbs'
+})
 
 const app = express();
 
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({ extended: true })); // forms
+app.use('/static', express.static('static'));
 app.get('/', (req, res) => {
     res.send('its work!');
 });
 
-app.listen(6161);
-
-console.log('Server is listenning on port 6161...')
+app.listen(6161, () => console.log('Server is listenning on port 6161...'));
