4d300dcc68d7ce92a04592e4e63cd5306fe38fc9
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-20 18:26:06 +0300

    init

--- expressJS-and-templating/workshop-vkostad/controllers/catalogController.js
+++ expressJS-and-templating/workshop-vkostad/controllers/catalogController.js
@@ -1,15 +1,26 @@
+const { getAll, getById } = require('../services/dataService');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('catalog');
+    const rooms = getAll();
+    res.render('catalog', {
+        title: 'Catalog',
+        rooms
+    });
 });
 
 router.get('/:id', (req, res) => {
     const id = req.params.id;
-    res.render('details', {
-        title: 'Details',
-        itemId: id
-    });
+    const item = getById(id);
+    if(item){
+        res.render('details', {
+            title: 'Details',
+            item
+        });
+    } else {
+        res.redirect('/404')
+    }
 });
 
 module.exports = router;
---
+++ expressJS-and-templating/workshop-vkostad/services/dataService.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const fileName = './models/data.json';
+
+const data = JSON.parse(fs.readFileSync(fileName));
+
+async function persist(){
+    return new Promise((res, rej) => {
+        fs.writeFile(fileName, JSON.stringify(data), (err) => {
+            if(err == null){
+                res();
+            } else {
+                rej(err);
+            }
+        });
+    });
+}
+
+//persist() - Създаваме нов промис който приема дефолтните параметри resolve and reject, в промиса записваме във файл[writeFile] - "fileName", 
+//масива "data" като го превръщаме в JSON. Метода writeFile дефолтно приема само един параметър - error, който ще бъде различен от null
+//ако данните не са записани успешно във файла. Затова е проверката ако error == null, тогава resolve-ваме промиса, а в противен случай го reject-ваме
+//като подаваме грешката 
+
+function getAll(){
+    return data;
+}
+
+function getById(id){
+    return data.find(x => x.id == id);
+}
+
+module.exports = {
+    getAll,
+    getById
+}
