0784a505487f9fdf639930cf64b760775391199f
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-27 10:11:20 +0300

    express-session-demo

--- session-and-authentication/cookies/index.js
+++ session-and-authentication/cookies/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -14,7 +21,7 @@ app.get('/', (req, res) => {
     </nav>
     <h1>Hello</h1>
     `);
-    
+
 });
 
 app.get('/login', (req, res) => {
@@ -29,13 +36,17 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     const data = {
-        username: 'Ilich'
+        username: 'ilich'
     }
 
-    if(username == 'ilich' && password == '123'){ 
-        
-        res.cookie('auth', JSON.stringify(data));
+    if (username == 'ilich' && password == '123') {
 
+        res.cookie('auth', JSON.stringify(data));
+        
+        req.session.username = 'ilich';
+        req.session.privateInfo = 'Some private info';
+        
+        console.log(req.session);
         res.redirect('/profile');
     } else {
         res.status(401).send('<h1>Wrong pass!</h1>');
@@ -45,8 +56,9 @@ app.post('/login', (req, res) => {
 app.get('/profile', (req, res) => {
     const authData = req.cookies['auth'];
 
-    if(authData){
-        const {username} = JSON.parse(authData);
+    if (authData) {
+
+        const { username } = JSON.parse(authData);
 
         res.send(`<h1>Hello ${username}</h1>`)
     } else {
