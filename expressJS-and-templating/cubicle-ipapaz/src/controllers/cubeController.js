308ecc1cd21ee4d395ea8f2a58224683395b100d
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-28 17:28:51 +0300

    added search functionality

--- expressJS-and-templating/cubicle-ipapaz/src/controllers/cubeController.js
+++ expressJS-and-templating/cubicle-ipapaz/src/controllers/cubeController.js
@@ -18,10 +18,14 @@ cubeController.get('/details/:cubeId', (req, res) => {
     const cubeId = Number(req.params.cubeId);
 
     if(!cubeId){
-        return res.redirect('404');
+        return res.redirect('/404');
     }
 
     const cube = db.cubes.find(x => x.id == cubeId);
+    
+    if(!cube){
+        return res.redirect('/404');
+    }
 
     res.render('details', { cube });
 });
--- expressJS-and-templating/cubicle-ipapaz/src/controllers/homeController.js
+++ expressJS-and-templating/cubicle-ipapaz/src/controllers/homeController.js
@@ -2,12 +2,32 @@ const homeController = require('express').Router();
 const db = require('../db.json');
 
 homeController.get('/', (req, res) => {
+    const {search, from, to} = req.query;
     
-    res.render('index', {cubes: db.cubes});
+    let cubes = db.cubes;
+
+    if (search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (from) {
+        cubes = cubes.filter(cube => Number(cube.difficultyLevel) >= from);
+    }
+
+    if (to) {
+        cubes = cubes.filter(cube => Number(cube.difficultyLevel) <= to);
+    }
+
+
+    res.render('index', { cubes, search, from, to });
 });
 
 homeController.get('/about', (req, res) => {
     res.render('about');
+});
+
+homeController.get('/404', (req, res) => {
+    res.render('404');
 });
 
 module.exports = homeController;
